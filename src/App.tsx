d969678ff094f11300112e308889280cfddcd96b
eyodohan <eyodohan@gmail.com>
2022-01-12 16:28:11 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 
 import './App.css';
+import { Private } from './components/auth/Private';
+import { Profile } from './components/auth/Profile';
 import { Button } from './components/Button';
+import Counter from './components/class/Counter';
 import { Container } from './components/Container';
 import { Box } from './components/context/Box';
 import { ThemeContextProvider } from './components/context/ThemeContext';
 import { User } from './components/context/User';
 import { UserContextProvider } from './components/context/UserContext';
+import { List } from './components/generics/List';
 import { Greet } from './components/Greet';
 import { Heading } from './components/Heading';
 import { Input } from './components/Input';
 import { Oscar } from './components/Oscar';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
-import { Counter } from './components/state/Counter';
+import { Mutable } from './components/ref/Mutable';
+// import { Counter } from './components/state/Counter';
 import { Status } from './components/Status';
 
 function App() {
@@ -53,9 +58,20 @@ function App() {
       {/* <ThemeContextProvider>
         <Box/>
       </ThemeContextProvider> */}
-      <UserContextProvider>
+      {/* <UserContextProvider>
         <User/>
-      </UserContextProvider>
+      </UserContextProvider> */}
+      {/* <Mutable /> */}
+      {/* <Counter message="The count value is"/> */}
+      {/* <Private isLoggedIn={true} component={Profile} /> */}
+      {/* <List items={["Batman","Superman","Wonder Woman"]} onClick={item=>console.log(item)}/>
+      <List items={[1,2,3]} onClick={item=>console.log(item)}/> */}
+      <List items={[
+        {id:1,first:"Bruce",last:"Wayne",},
+        {id:2,first:"Clark",last:"Kent",},
+        {id:3,first:"Princess",last:"Diana",}
+        ,]} 
+        onClick={item=>console.log(item)}/>
     </div>
   );
 }
